2b0df8b3102598873ce3fa6218184cc5ef1010a7
UnmeinoZero <124483264+UnmeinoZero@users.noreply.github.com>
2023-04-14 10:53:14 +0800

    缓存优化

---
+++ src/main/java/com/uzero/reggie/config/RedisConfig.java
@@ -0,0 +1,26 @@
+package com.uzero.reggie.config;
+
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * @author 千叶零
+ * @version 1.0
+ * create 2023-04-14  10:44:39
+ */
+@Configuration
+public class RedisConfig extends CachingConfigurerSupport {
+
+    @Bean
+    public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<Object, Object> redisTemplate = new RedisTemplate<>();
+        //默认的Key序列化器为：JdkSerializationRedisSerializer
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setConnectionFactory(connectionFactory);
+        return redisTemplate;
+    }
+}
