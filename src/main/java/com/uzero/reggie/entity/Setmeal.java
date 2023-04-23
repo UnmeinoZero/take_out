b4a26b8c0fded12e70e8828d6c5ad86b7ed298d3
UnmeinoZero <124483264+UnmeinoZero@users.noreply.github.com>
2023-04-23 19:11:21 +0800

    远程部署

--- src/main/java/com/uzero/reggie/entity/Setmeal.java
+++ src/main/java/com/uzero/reggie/entity/Setmeal.java
@@ -16,71 +16,71 @@ import java.time.LocalDateTime;
  * 套餐
  */
 @Data
-@ApiModel("套餐")
+@ApiModel(description = "套餐")
 public class Setmeal implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
-    @ApiModelProperty("主键")
+    @ApiModelProperty(value = "主键")
     private Long id;
 
 
     //分类id
-    @ApiModelProperty("分类id")
+    @ApiModelProperty(value = "分类id")
     private Long categoryId;
 
 
     //套餐名称
-    @ApiModelProperty("套餐名称")
+    @ApiModelProperty(value = "套餐名称")
     private String name;
 
 
     //套餐价格
-    @ApiModelProperty("套餐价格")
+    @ApiModelProperty(value = "套餐价格")
     private BigDecimal price;
 
 
     //状态 0:停用 1:启用
-    @ApiModelProperty("状态")
+    @ApiModelProperty(value = "状态")
     private Integer status;
 
 
     //编码
-    @ApiModelProperty("编码")
+    @ApiModelProperty(value = "编码")
     private String code;
 
 
     //描述信息
-    @ApiModelProperty("描述信息")
+    @ApiModelProperty(value = "描述信息")
     private String description;
 
 
     //图片
-    @ApiModelProperty("图片")
+    @ApiModelProperty(value = "图片")
     private String image;
 
 
     @TableField(fill = FieldFill.INSERT)
-    @ApiModelProperty("创建时间")
+    @ApiModelProperty(value = "创建时间")
     private LocalDateTime createTime;
 
 
     @TableField(fill = FieldFill.INSERT_UPDATE)
-    @ApiModelProperty("修改时间")
+    @ApiModelProperty(value = "修改时间")
     private LocalDateTime updateTime;
 
 
     @TableField(fill = FieldFill.INSERT)
-    @ApiModelProperty("创建人")
+    @ApiModelProperty(value = "创建人")
     private Long createUser;
 
 
     @TableField(fill = FieldFill.INSERT_UPDATE)
-    @ApiModelProperty("修改人")
+    @ApiModelProperty(value = "修改人")
     private Long updateUser;
 
 
     //是否删除
-    @ApiModelProperty("是否删除")
+    @ApiModelProperty(value = "是否删除")
     private Integer isDeleted;
 }
